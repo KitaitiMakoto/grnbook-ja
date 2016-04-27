43b5a43fd6b8fe35a5ba83f25da631598458c759
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-04-27 11:59:34 +0900

    Add fulltext search feature

---
+++ ch02/search/table.php
@@ -0,0 +1,53 @@
+<?php
+namespace PDFSearch;
+
+class Table
+{
+    const INSERT =
+        "INSERT INTO `pdfs` (file, title, content)
+         VALUES(:file, :title, :content);";
+    const SELECT = "SELECT * FROM `pdfs` ORDER BY `id`;";
+    const SEARCH = "SELECT * FROM `pdfs` WHERE MATCH(content) AGAINST(:query);";
+
+    protected $pdo;
+
+    public function __construct($dsn, $username, $password)
+    {
+        $this->pdo = new \PDO($dsn, $username, $password);
+    }
+
+    public function insert(Upload $upload)
+    {
+        $params = [
+            ':file'    => $upload->getName(),
+            ':title'   => $upload->getTitle(),
+            ':content' => $upload->getContent()
+        ];
+        $sth = $this->pdo->prepare(self::INSERT);
+        $isSucceeded = $sth->execute($params);
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+    }
+
+    public function records()
+    {
+        $sth = $this->pdo->prepare(self::SELECT);
+        $isSucceeded = $sth->execute();
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+        return $sth->fetchAll();
+    }
+
+    public function search($query)
+    {
+        $sth = $this->pdo->prepare(self::SEARCH);
+        $params = ['query' => $query];
+        $isSucceeded = $sth->execute($params);
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+        return $sth->fetchAll();
+    }
+}
--- src/ch02.re
+++ src/ch02.re
@@ -279,3 +279,28 @@ Dockerイメージの中には、既にPopplerとphp-popplerがインストー�
 //}
 
 == 全文検索機能の作成
+
+ようやく、全文検索機能の実装です。実は、これもMroongaならではの特別なことは必要なく、通常のMySQLの全文検索機能、即ち@<code>{MATCH() ... AGAINST}構文を使用するだけです。テーブル作成時に、ストレージエンジンにMroongaを選択しておけば、他の操作は通常のMySQLと同じように実行できるようにデザインされているのです。
+
+今の「登録済みPDF一覧」の上に検索フォームを作成し、検索を実行した場合にはフォームの下に検索結果を表示するようにしてみましょう。そのために、クラス@<code>{\PDFSearch\Table}クラスに検索用のメンバー関数を追加します。
+
+//list[ch02/search/search.php][search.php][php]{
+#@mapfile(ch02/search/table.php)
+#@end
+//}
+
+@<code>{SEARCH}定数と@<code>{search}メンバー関数を追加しています。
+
+先ほど言ったような検索フォームと結果のエリアを追加すると、index.phpはこうなります。
+
+//list[ch02/search/index.php][index.php][php]{
+#@mapfile(ch02/search/index.php)
+#@end
+//}
+
+SQL一文で済むので簡単ですね。では、検索してみましょう。
+
+//image[search][全文検索結果の表示]{
+//}
+
+以上で、基本的な機能は完成です。以降では、Mroongaに特徴的な幾つかの機能を紹介し、実装していきます。
