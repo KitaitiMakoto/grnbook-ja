fad6c134b542a6018842cbf8b4ab4c66bd8e26eb
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-05-06 08:29:45 +0900

    Add PHP scripts for highlighting

---
+++ ch02/highlight/index.php
@@ -0,0 +1,97 @@
+<?php
+// Dockerイメージ内にComposerでインストール済みのライブラリーを読み込む
+require_once 'vendor/autoload.php';
+require_once __DIR__ . '/upload.php';
+require_once __DIR__ . '/table.php';
+
+define('DBNAME', 'pdfsearch');
+
+$dsn = 'mysql:host=localhost;dbname=' . DBNAME . ';charset=utf8';
+$table = new \PDFSearch\Table($dsn, 'root', '');
+
+function h($string, $flags = ENT_QUOTES, $encoding = 'UTF-8')
+{
+    return htmlspecialchars($string, $flags, $encoding);
+}
+
+// 検索処理
+$searchQuery = null;
+if ($_SERVER['REQUEST_METHOD'] === 'GET' &&
+    array_key_exists('q', $_GET) && $_GET['q']) {
+    $searchQuery = $_GET['q'];
+    $searchResult = $table->search($searchQuery);
+}
+
+// ファイルアップロード処理
+if ($_SERVER['REQUEST_METHOD'] === 'POST' &&
+    array_key_exists('pdf', $_FILES)) {
+    $uploads = \PDFSearch\Upload::fromFilesInfo($_FILES['pdf']);
+    try {
+        foreach ($uploads as $upload) {
+            $table->insert($upload);
+        }
+        header('Location: .');
+    } catch (\RuntimeException $e) {
+        header('HTTP', true, 500);
+        header('content-type: text/plain');
+        echo $e;
+    }
+    exit;
+}
+
+$records = $table->records();
+
+?><!doctype html>
+<title>PDF Search</title>
+<style>
+  .keyword {
+    font-weight: bold;
+    color: red;
+  }
+</style>
+<h1>PDF Search</h1>
+
+<h2>検索</h2>
+<form method="get">
+  <input name="q" type="search" value="<?php echo h($searchQuery); ?>">
+  <input type="submit" value="検索">
+</form>
+
+<?php if ($searchResult && count($searchResult) > 0): ?>
+<h2>「<?php echo h($searchQuery); ?>」を含むPDF</h2>
+<table>
+  <tr>
+    <th>ファイル名</th>
+    <th>タイトル</th>
+    <th>内容</th>
+  </tr>
+  <?php foreach ($searchResult as $record): ?>
+    <tr>
+      <td><?php echo h($record['file']); ?></td>
+      <td><?php echo h($record['title']); ?></td>
+      <td><?php echo $record['snippets']; ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
+<?php endif; ?>
+
+<h2>登録済みPDF一覧</h2>
+<table>
+  <tr>
+    <th>ファイル名</th>
+    <th>タイトル</th>
+    <th>内容</th>
+  </tr>
+  <?php foreach ($records as $record): ?>
+    <tr>
+      <td><?php echo h($record['file']); ?></td>
+      <td><?php echo h($record['title']); ?></td>
+      <td><?php echo h(mb_substr($record['content'], 0, 120)); ?>&hellip;&hellip;</td>
+    </tr>
+  <?php endforeach; ?>
+</table>
+
+<form enctype="multipart/form-data" method="post">
+  <input name="pdf[]" type="file" multiple>
+  <input type="submit">
+</form>
---
+++ ch02/highlight/table.php
@@ -0,0 +1,68 @@
+<?php
+namespace PDFSearch;
+
+class Table
+{
+    const INSERT =
+        "INSERT INTO `pdfs` (file, title, content)
+         VALUES(:file, :title, :content);";
+    const SELECT = "SELECT * FROM `pdfs` ORDER BY `id`;";
+    const SEARCH = <<<EOS
+SELECT file, title, mroonga_snippet(
+  content,
+  120,
+  3,
+  'utf8_general_ci',
+  1,
+  1,
+  '<p>',
+  '&hellip;&hellip;</p>',
+  :query,
+  '<span class="keyword">',
+  '</span>'
+  ) AS snippets FROM `pdfs`
+WHERE MATCH(content) AGAINST(:query IN BOOLEAN MODE);
+EOS;
+
+    protected $pdo;
+
+    public function __construct($dsn, $username, $password)
+    {
+        $this->pdo = new \PDO($dsn, $username, $password);
+    }
+
+    public function insert(Upload $upload)
+    {
+        $params = [
+            ':file'    => $upload->getName(),
+            ':title'   => $upload->getTitle(),
+            ':content' => $upload->getContent()
+        ];
+        $sth = $this->pdo->prepare(self::INSERT);
+        $isSucceeded = $sth->execute($params);
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+    }
+
+    public function records()
+    {
+        $sth = $this->pdo->prepare(self::SELECT);
+        $isSucceeded = $sth->execute();
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+        return $sth->fetchAll();
+    }
+
+    public function search($query)
+    {
+        $sth = $this->pdo->prepare(self::SEARCH);
+        $params = [':query' => $query];
+        $isSucceeded = $sth->execute($params);
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+        return $sth->fetchAll();
+    }
+}
