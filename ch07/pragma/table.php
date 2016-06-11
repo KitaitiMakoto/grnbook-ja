870c546f8cef1c1091a45fb0de11fd72df77bf24
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-06-11 19:34:56 +0900

    Add sample code for pragma

---
+++ ch07/pragma/table.php
@@ -0,0 +1,55 @@
+<?php
+namespace PDFSearch;
+
+class Table
+{
+    const INSERT =
+        "INSERT INTO `pdfs` (file, title, content)
+         VALUES(:file, :title, :content);";
+    const SELECT = "SELECT * FROM `pdfs` ORDER BY `id`;";
+    const SEARCH = <<<EOS
+SELECT * FROM `pdfs` WHERE MATCH(title, content) AGAINST(:query_with_pragma IN BOOLEAN MODE);
+EOS;
+
+    protected $pdo;
+
+    public function __construct($dsn, $username, $password)
+    {
+        $this->pdo = new \PDO($dsn, $username, $password);
+    }
+
+    public function insert(Upload $upload)
+    {
+        $params = [
+            ':file'    => $upload->getName(),
+            ':title'   => $upload->getTitle(),
+            ':content' => $upload->getContent()
+        ];
+        $sth = $this->pdo->prepare(self::INSERT);
+        $isSucceeded = $sth->execute($params);
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+    }
+
+    public function records()
+    {
+        $sth = $this->pdo->prepare(self::SELECT);
+        $isSucceeded = $sth->execute();
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+        return $sth->fetchAll();
+    }
+
+    public function search($query)
+    {
+        $sth = $this->pdo->prepare(self::SEARCH);
+        $params = [':query_with_pragma' => '*D+' . $query];
+        $isSucceeded = $sth->execute($params);
+        if (! $isSucceeded) {
+            throw new \RuntimeException(implode(PHP_EOL, $sth->errorInfo()));
+        }
+        return $sth->fetchAll();
+    }
+}
