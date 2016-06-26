fa8b5f4ec8ecb51b02b2cf7b096011173d52c01b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-06-26 21:54:15 +0900

    Fix a typo

--- ch04/insert/index.php
+++ ch04/insert/index.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' &&
     $uploads = \PDFSearch\Upload::fromFilesInfo($_FILES['pdf']);
     try {
         foreach ($uploads as $upload) {
-            $tableb->insert($upload);
+            $table->insert($upload);
         }
         header('Location: .');
     } catch (\RuntimeException $e) {
